355f43bb725e4e92678d7c0b8a918cd3ea836c8a
Lucrecia Bazán <lucreciadenisebazan@gmail.com>
2025-10-13 16:22:55 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Este es el primer cambio");
         System.out.printf("este es el cambio ale ");
-        System.out.println(" puto el que lee ");
+        System.out.println(" puto el que lee jejejejeje");
         System.out.println("4° cambio");
     }
 }
